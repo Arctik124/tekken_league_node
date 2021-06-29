bd192bd89e20dd8576cc6d0193bed64bed86da5d
Artyom <artvit12@gmail.com>
2021-06-29 15:35:41 +0600

    Added /api/scoreboard

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,31 @@ router.get('/dashboard', ensureAuthenticated, (req, res) => {
     });
 })
 
+router.get('/api/scoreboard', async(req, res) => {
+    console.log('accept')
+    var page = 1;
+    var limit = 20;
+    if (typeof req.query.page != 'undefined') page = parseInt(req.query.page);
+    if (typeof req.query.limit != 'undefined') limit = parseInt(req.query.limit);
+    const startIndex = (page - 1) * limit;
+
+    const n_players = await User.find().countDocuments();
+    const players = await User.find().sort('-mmr').skip(startIndex).limit(limit);
+    console.log(n_players)
+    let players_json = []
+
+    players.forEach(player => {
+        players_json.push({
+            username: player.name,
+            score: player.mmr
+        })
+    });
+
+    res.json({
+        count: n_players,
+        players: players_json
+    })
+
+})
+
 module.exports = router;
